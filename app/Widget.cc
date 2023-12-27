035f54d9268e4763eaf1c58d86f2b284fa7a8596
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2023-12-27 18:43:03 +0200

    Add people

--- app/Widget.cc
+++ app/Widget.cc
@@ -2,7 +2,7 @@
 #include "./ui_widget.h"
 
 Widget::Widget(QWidget *parent)
-    : QWidget{parent}
+    : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
@@ -10,4 +10,47 @@ Widget::Widget(QWidget *parent)
     // connect(ui->pushButton, &QPushButton::clicked, this, [=](){
     //     ui->outputLabel->setText(person.sayHello());
     // });
+    m_people.setWeight(ui->weightSpinBox->value());
+    m_time = ui->timeSpinBox->value();
+    m_speed = ui->speedSpinBox->value();
+
+    connect(ui->weightSpinBox, &QDoubleSpinBox::valueChanged, this, &Widget::weightChanged);
+    connect(ui->timeSpinBox, &QDoubleSpinBox::valueChanged, this, &Widget::timeChanged);
+    connect(ui->speedSpinBox, &QDoubleSpinBox::valueChanged, this, &Widget::speedChanged);
+
+    connect(this, &Widget::caloryCountChanged, this, [=](){
+        ui->caloryLabel->setText(QString::number(m_caloryCount));
+    });
+
+    calculateCalories();
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::calculateCalories()
+{
+    m_caloryCount = ((0.0215 * (m_speed * m_speed * m_speed)) - (0.1765 *(m_speed * m_speed)) +
+                     (0.8710 * m_speed) + 1.4577) * m_people.getWeight() * m_time;
+    emit caloryCountChanged();
+}
+
+void Widget::weightChanged(double weight)
+{
+    m_people.setWeight(weight);
+    calculateCalories();
+}
+
+void Widget::timeChanged(double time)
+{
+    m_time = time;
+    calculateCalories();
+}
+
+void Widget::speedChanged(double speed)
+{
+    m_speed = speed;
+    calculateCalories();
 }
--- app/Widget.h
+++ app/Widget.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include "Person.h"
+#include "People.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,15 +14,28 @@ QT_END_NAMESPACE
 class Widget : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void calculateCalories();
+    void weightChanged(double weight);
+    void timeChanged(double time);
+    void speedChanged(double speed);
 
 signals:
+    void caloryCountChanged();
 
 private:
     Ui::Widget *ui;
-
     Person person;
+    People m_people;
+
+    double m_time;
+    double m_speed;
+    double m_caloryCount;
 };
 
 #endif // WIDGET_H
